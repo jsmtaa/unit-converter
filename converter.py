64b7bc8272a416f6213f501fd7b739556bf3d7fa
jsmtaa <200186120+jsmtaa@users.noreply.github.com>
2026-01-21 20:33:19 +0800

    Initial converter implementation

---
+++ converter.py
@@ -0,0 +1,18 @@
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+
+def main():
+    converted = convert(1, "m", "km")
+    print(format_scientific(converted))
+
+# Simple conversion
+def convert(value, from_unit, to_unit):
+    return float(value) * ureg.__call__(from_unit).to(ureg.__call__(to_unit)).magnitude
+
+# Scientific notation
+def format_scientific(value):
+    return f"{value:.1e}"
+
+if __name__ == "__main__":
+    main()
